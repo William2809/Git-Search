36b5a163427b4cd0025d448a7a07214c41cd2b3b
William Huang <william.huang001@binus.ac.id>
2022-07-30 00:31:11 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -5,31 +5,38 @@ import NotFound from './components/pages/NotFound';
 import Navbar from './components/layouts/Navbar';
 import Footer from './components/layouts/Footer';
 import { GithubProvider } from './context/github/GithubContext';
+import { AlertProvider } from './context/alert/AlertContext';
+import Alert from './components/layouts/Alert';
+import User from './components/pages/User'
 
 function App() {
   return (
     <GithubProvider>
+      <AlertProvider>
 
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/notfound' element={<NotFound />} />
-              <Route path='/*' element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
+              <Alert />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/User/:login' element={<User />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
 
-        {/* <Routes>
+          {/* <Routes>
         <Route path="/" element={
         } />
 
     </Routes> */}
-      </Router>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
